34dd5f22fdc94a8d4c862f4901f5089d37991eea
Leart-Loen <learturely@gmail.com>
2023-10-05 04:15:10 +0800

    更新依赖。

--- src/main/java/rt/lea/Main.java
+++ src/main/java/rt/lea/Main.java
@@ -1,16 +1,21 @@
 package rt.lea;
 
 import kotlinx.coroutines.flow.Flow;
+import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.contact.AnonymousMember;
 import net.mamoe.mirai.contact.ContactList;
 import net.mamoe.mirai.contact.Friend;
+import net.mamoe.mirai.contact.Member;
 import net.mamoe.mirai.contact.NormalMember;
 import net.mamoe.mirai.contact.Group;
+import net.mamoe.mirai.contact.Stranger;
+import net.mamoe.mirai.contact.active.MemberMedalType;
 import net.mamoe.mirai.contact.file.AbsoluteFile;
 import net.mamoe.mirai.contact.file.AbsoluteFolder;
 import net.mamoe.mirai.contact.file.AbsoluteFileFolder;
 import net.mamoe.mirai.contact.friendgroup.FriendGroup;
 import net.mamoe.mirai.contact.friendgroup.FriendGroups;
+import net.mamoe.mirai.data.GroupHonorType;
 import net.mamoe.mirai.data.RequestEventData;
 import net.mamoe.mirai.event.Event;
 import net.mamoe.mirai.event.events.MemberJoinRequestEvent;
@@ -23,10 +28,12 @@ import net.mamoe.mirai.message.data.VipFace;
 import net.mamoe.mirai.message.data.OnlineMessageSource;
 import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.Image;
+
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Mirai!\u0014\u0167\u0014\u0166\u2588\uD83C\uDFB2");
+        System.out.println("Hello Mirai!\u0014\u0167\u0014\u0166\uD83C\uDFB2");
     }
 }
